596afa980c8f150c4e6709d32f29140f313df2e8
ndgndg91 <ndgndg91@gmail.com>
2025-07-13 21:13:56 +0900

    basic calculator

--- algorithm/src/main/kotlin/com/ndgndg91/basiccalculator/BasicCalculator.kt
+++ algorithm/src/main/kotlin/com/ndgndg91/basiccalculator/BasicCalculator.kt
@@ -1,11 +1,19 @@
 package com.ndgndg91.basiccalculator
 
+import java.util.*
+
 class BasicCalculator {
 
     fun calculate(s: String): Int {
+        val tokens = tokenize(s)
+        val postfix = postfix(tokens)
+        return evaluate(postfix)
+    }
+
+    fun tokenize(s: String): List<String> {
         val s = s.replace(" ", "")
-        var result = 0
-        var sign = 1
+        val tokens = mutableListOf<String>()
+
         var i = 0
         while (i <= s.lastIndex) {
             when {
@@ -15,39 +23,104 @@ class BasicCalculator {
                         number += s[i]
                         i++
                     }
-//                    println("$number is a digit")
-                    result += +number.toInt() * sign
+                    tokens.add(number)
                     continue
                 }
-                s[i] == '+' -> {
-//                    println("${s[i]} is not a number")
-                    sign = 1
-                }
-                s[i] == '-' -> {
-//                    println("${s[i]} isn not a number")
-                    sign = -1
-                }
+                s[i] == '+' -> tokens.add(s[i].toString())
+                s[i] == '-' -> tokens.add(s[i].toString())
+                s[i] == '*' -> tokens.add(s[i].toString())
+                s[i] == '/' -> tokens.add(s[i].toString())
+                s[i] == '(' -> tokens.add(s[i].toString())
+                s[i] == ')' -> tokens.add(s[i].toString())
             }
             i++
         }
 
-        return result
+        return tokens
     }
+
+    fun postfix(tokens: List<String>): List<String> {
+        val results = mutableListOf<String>()
+        val stack = Stack<String>()
+        for (token in tokens) {
+            when (token) {
+                "+", "-", "/", "*" -> {
+                    while (stack.isNotEmpty() && stack.peek() != "(" && precedence(stack.peek()) >= precedence(token)) {
+                        results.add(stack.pop())
+                    }
+
+                    stack.push(token)
+                }
+                "(" -> stack.push(token)
+                ")" -> {
+                    while (stack.isNotEmpty() && stack.peek() != "(") {
+                        results.add(stack.pop())
+                    }
+
+                    // ")" 제거
+                    if (stack.isNotEmpty()) {
+                        stack.pop()
+                    }
+                }
+                else -> results.add(token)
+            }
+        }
+
+        while (stack.isNotEmpty()) {
+            results.add(stack.pop())
+        }
+
+        return results
+    }
+
+    private fun precedence(token: String): Int {
+        return when (token) {
+            "+", "-" -> 1 // 낮은 우선순위
+            "*", "/" -> 2 // 높은 우선순위
+            else -> 0     // 괄호('(') 또는 그 외의 경우는 가장 낮은 순위로 처리
+        }
+    }
+
+    fun evaluate(postfix: List<String>): Int {
+        val stack = Stack<Int>()
+        for (token in postfix) {
+            when {
+                token.toIntOrNull() != null -> { stack.push(token.toInt()) }
+                token == "+" -> {
+                    val b = stack.pop()
+                    val a = stack.pop()
+                    stack.push(a + b)
+                }
+                token == "-" -> {
+                    val b = stack.pop()
+                    val a = stack.pop()
+                    stack.push(a - b)
+                }
+                token == "*" -> {
+                    val b = stack.pop()
+                    val a = stack.pop()
+                    stack.push(a * b)
+                }
+                token == "/" -> {
+                    val b = stack.pop()
+                    val a = stack.pop()
+                    stack.push(a / b)
+                }
+            }
+
+        }
+        return stack.pop()
+    }
+
 }
 
 fun main() {
     val basicCalculator = BasicCalculator()
-    check(basicCalculator.calculate(" ") == 0)
-    check(basicCalculator.calculate("1 + 12") == 13)
-    check(basicCalculator.calculate("1+12") == 13)
-    check(basicCalculator.calculate("1 - 12") == -11)
-    check(basicCalculator.calculate("1-12") == -11)
+    check(basicCalculator.tokenize("1 - 12+15-30+15") == listOf("1", "-", "12", "+", "15", "-", "30", "+", "15"))
+    check(basicCalculator.tokenize("1 - (12+15)-30+15") == listOf("1", "-", "(", "12", "+", "15", ")", "-", "30", "+", "15"))
 
-    check(basicCalculator.calculate("1 + 12 + 2") == 15)
-    check(basicCalculator.calculate("1+12+2") == 15)
-    check(basicCalculator.calculate("1 - 12 - 4") == -15)
-    check(basicCalculator.calculate("1-12-4") == -15)
-
-    check(basicCalculator.calculate("1 - 12+15-30+15") == -11)
+    check(basicCalculator.postfix(basicCalculator.tokenize("1 - (12+15)-30+15")) == listOf("1","12","15","+","-","30", "-","15","+"))
+    check(basicCalculator.evaluate(basicCalculator.postfix(basicCalculator.tokenize("1 - (12+15)-30+15"))) == -41)
+    check(basicCalculator.calculate("1 - (12+15)-30+15") == -41)
 }
 
