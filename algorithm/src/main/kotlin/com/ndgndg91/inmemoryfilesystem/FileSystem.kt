cfde48b674918c8713c883a79951a196fc7748e5
giri.nam <giri.nam@coinone.com>
2025-07-17 15:21:22 +0900

    implementation file system

--- algorithm/src/main/kotlin/com/ndgndg91/inmemoryfilesystem/FileSystem.kt
+++ algorithm/src/main/kotlin/com/ndgndg91/inmemoryfilesystem/FileSystem.kt
@@ -6,8 +6,8 @@ class FileSystem {
 
     class TrieNode {
         val children = TreeMap<String, TrieNode>()
-        val isFile = false
-        val content = ""
+        var isFile = false
+        val content = StringBuilder()
     }
 
     private val root = TrieNode()
@@ -26,19 +26,53 @@ class FileSystem {
     }
 
 
-    fun ls(path: String) {
+    fun ls(path: String): List<String> {
+        val current = findNode(path)
+        if (current == null) return emptyList()
 
+        return current.children.keys.map { "$path/$it" }
     }
 
     fun mkdir(path: String) {
+        if (path == "/") return
 
+        val components = path.split("/").drop(1)
+        var current = root
+
+        for (component in components) {
+            current = current.children.getOrPut(component) { TrieNode() }
+        }
     }
 
     fun addContentToFile(path: String, content: String) {
-
+        val split = path.split("/")
+        val directoryPath = split.dropLast(1).joinToString("/")
+        val current = findNode(directoryPath)
+        if (current == null) return
+        val node = TrieNode()
+        node.isFile = true
+        node.content.append(content)
+        current.children.put(split.last(), node)
     }
 
     fun readContentFromFile(path: String): String {
-        return ""
+        val current = findNode(path)
+        if (current == null) return ""
+        return if (current.isFile) current.content.toString()
+        else ""
     }
+}
+
+fun main() {
+    val fs = FileSystem()
+    fs.mkdir("/home/ndgndg91")
+    println(fs.ls("/home/ndgndg91"))
+    fs.mkdir("/home/ndgndg91/a")
+    fs.mkdir("/home/ndgndg91/b")
+    fs.mkdir("/home/ndgndg91/c")
+    println(fs.ls("/home/ndgndg91"))
+
+    println("/home/ndgndg91/input.txt".split("/").dropLast(1).joinToString("/"))
+    fs.addContentToFile("/home/ndgndg91/input.txt", "hello file system")
+    println(fs.readContentFromFile("/home/ndgndg91/input.txt"))
 }
