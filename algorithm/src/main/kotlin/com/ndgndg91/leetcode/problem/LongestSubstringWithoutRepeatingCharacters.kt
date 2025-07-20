2372ea8ecab7ebbc272daa34174572182361acda
ndgndg91 <ndgndg91@gmail.com>
2025-07-20 21:31:06 +0900

    longest substring without repeating characters

---
+++ algorithm/src/main/kotlin/com/ndgndg91/leetcode/problem/LongestSubstringWithoutRepeatingCharacters.kt
@@ -0,0 +1,34 @@
+package com.ndgndg91.leetcode.problem
+
+import kotlin.math.max
+
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var left = 0
+        var right = 0
+        var answer = 0
+        val set = HashSet<Char>()
+
+        while (right < s.length) {
+            while (s[right] in set) {
+                set.remove(s[left])
+                left++
+            }
+
+            set.add(s[right])
+
+            answer = max(answer, right - left + 1)
+            right++
+        }
+
+        return answer
+    }
+}
+
+fun main() {
+    val solution = Solution()
+//    check(solution.lengthOfLongestSubstring("abcabcbb") == 3)
+//    check(solution.lengthOfLongestSubstring("bbbbb") == 1)
+//    check(solution.lengthOfLongestSubstring("pwwkew") == 3)
+    check(solution.lengthOfLongestSubstring("dvdf") == 3)
+}
