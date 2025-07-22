f7a88af26924cf9ec7e0343684a649e77764c488
ndgndg91 <ndgndg91@gmail.com>
2025-07-22 22:06:23 +0900

    optimize merge two sorted list

--- algorithm/src/main/kotlin/com/ndgndg91/leetcode/problem/easy/MergeTwoSortedList.kt
+++ algorithm/src/main/kotlin/com/ndgndg91/leetcode/problem/easy/MergeTwoSortedList.kt
@@ -15,6 +15,34 @@ class MergeTwoSortedList {
     }
 
     class Solution {
+        fun mergeTwoLists2(list1: ListNode?, list2: ListNode?): ListNode? {
+            // Create a dummy head node to simplify the logic of adding the first element
+            val dummyHead = ListNode(0)
+            var current: ListNode? = dummyHead
+
+            var p1 = list1
+            var p2 = list2
+
+            while (p1 != null && p2 != null) {
+                if (p1.`val` <= p2.`val`) {
+                    current?.next = p1
+                    p1 = p1.next
+                } else {
+                    current?.next = p2
+                    p2 = p2.next
+                }
+                current = current?.next
+            }
+
+            // If one list is exhausted, append the rest of the other list
+            if (p1 != null) {
+                current?.next = p1
+            } else if (p2 != null) {
+                current?.next = p2
+            }
+
+            return dummyHead.next // The ac
+        }
         fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
             val list = mutableListOf<Int>()
 
