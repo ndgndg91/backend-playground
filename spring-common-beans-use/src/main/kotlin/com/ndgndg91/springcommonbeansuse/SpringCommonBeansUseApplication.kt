7c657c28cb20776aa8d723820846f9a0444e3aed
ndgndg91 <ndgndg91@gmail.com>
2025-09-18 22:13:02 +0900

    spring common beans use 모듈 추가

---
+++ spring-common-beans-use/src/main/kotlin/com/ndgndg91/springcommonbeansuse/SpringCommonBeansUseApplication.kt
@@ -0,0 +1,11 @@
+package com.ndgndg91.springcommonbeansuse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCommonBeansUseApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCommonBeansUseApplication>(*args)
+}
---
+++ spring-common-beans-use/src/test/kotlin/com/ndgndg91/springcommonbeansuse/SpringCommonBeansUseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ndgndg91.springcommonbeansuse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCommonBeansUseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
